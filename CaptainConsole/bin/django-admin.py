c002f3731ec92f9abfda1b8bebb2fcaa4217a96b
Dagurh17 <dagurh17@ru.is>
2020-05-05 11:40:31 +0000

    nytt project
    Setti upp nýtt project í django

---
+++ CaptainConsole/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/dagurhilmarsson/Desktop/PyCharmProjects/CaptainConsole/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
